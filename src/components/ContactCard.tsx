5a7bf83bcd746c394bdaafe81464b9524d873bf6
anlmao125 <anlon.maoD06@gmail.com>
2025-07-17 12:17:11 -1000

    list contacts page mockup finished in 17 minutes

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import { Contact } from '@/lib/validationSchemas';
+
+interface Props {
+  contact: Contact;
+}
+
+const ContactCard = ({ contact }: Props) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={contact.image} width={75} rounded />
+    </Card.Header>
+    <Card.Body>
+      <Card.Title>
+        {contact.firstName}
+        {contact.lastName}
+      </Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">{contact.address}</Card.Subtitle>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCard;
